f796e5d4d9afb4b54ca60c259e7788c39fe9b5ed
Alex Deem <alexdeem@gmail.com>
2024-08-01 00:00:01 +1000

    support Foundation.Data

--- Tests/EnvironmentDecoderTests/EnvironmentDecoderCustomTypeTests.swift
+++ Tests/EnvironmentDecoderTests/EnvironmentDecoderCustomTypeTests.swift
@@ -1,7 +1,47 @@
+import EnvironmentDecoder
 import Foundation
 import Testing
 
 struct EnvironmentDecoderCustomTypeTests {
+    @Test func base64DataTest() throws {
+        #expect(try decode("", as: Data.self) == Data())
+        #expect(try decode("AQIDBAUGBwg=", as: Data.self) == Data([0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08]))
+        #expect(try decode("VGhlIHF1aWNrIGJyb3duIGZveCBqdW1wZWQgb3ZlciB0aGUgbGF6eSBkb2ch", as: Data.self)
+            == Data("The quick brown fox jumped over the lazy dog!".utf8))
+        #expect(try decode("VGhl,cXVpY2s=,YnJvd24=,Zm94", as: [Data].self)
+            == ["The", "quick", "brown", "fox"].map { Data($0.utf8) })
+        #expect(throws: Swift.DecodingError.self) {
+            try decode(" ", as: Data.self)
+        }
+        #expect(throws: Swift.DecodingError.self) {
+            try decode("invalid-base-64", as: Data.self)
+        }
+        #expect(throws: Swift.DecodingError.self) {
+            try decode(nil, as: Data.self)
+        }
+    }
+
+    @Test func delegatedDataTest() throws {
+        let decoder = EnvironmentDecoder(dataDecodingStrategy: .deferredToData)
+        #expect(try decode("", as: Data.self, with: decoder) == Data())
+        #expect(try decode("1,2,3,4,5,6,7,8", as: Data.self, with: decoder) == Data([0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08]))
+        #expect(try decode("0x1,0x2,0x3,0x4,0x5,0x6,0x7,0x8", as: Data.self, with: decoder) == Data([0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08]))
+        #expect(try decode("0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08", as: Data.self, with: decoder) == Data([0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08]))
+        #expect(try decode("0x54,0x68,0x65,0x20,0x71,0x75,0x69,0x63,0x6b,0x20,0x62,0x72,0x6f,0x77,0x6e,0x20,0x66,0x6f,0x78,0x20,0x6a,0x75,0x6d,0x70,0x65,0x64,0x20,0x6f,0x76,0x65,0x72,0x20,0x74,0x68,0x65,0x20,0x6c,0x61,0x7a,0x79,0x20,0x64,0x6f,0x67,0x21", as: Data.self, with: decoder)
+            == Data("The quick brown fox jumped over the lazy dog!".utf8))
+        #expect(try decode("VGhl,cXVpY2s=,YnJvd24=,Zm94", as: [Data].self)
+            == ["The", "quick", "brown", "fox"].map { Data($0.utf8) })
+        #expect(throws: Swift.DecodingError.self) {
+            try decode(" ", as: Data.self)
+        }
+        #expect(throws: Swift.DecodingError.self) {
+            try decode("invalid,int,array", as: Data.self)
+        }
+        #expect(throws: Swift.DecodingError.self) {
+            try decode(nil, as: Data.self)
+        }
+    }
+
     @Test func decimalTest() throws {
         let formatter = Decimal.FormatStyle(locale: .init(identifier: ""))
             .precision(.integerAndFractionLength(integerLimits: 1...10, fractionLimits: 0...10))
