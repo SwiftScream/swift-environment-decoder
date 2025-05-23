a6b7d293c8a1dea8a870cbe3eb8335cb3b111126
Alex Deem <alexdeem@gmail.com>
2025-05-23 14:00:24 +1000

    update swift-docc-plugin dependency to swiftlang

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
             targets: ["EnvironmentDecoder"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
+        .package(url: "https://github.com/swiftlang/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [
         .target(
